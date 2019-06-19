fc297b1154383a639e3a2fb262403e60fb2334c1
Brett Love <love.brett@gmail.com>
2019-06-19 09:10:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githublearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
